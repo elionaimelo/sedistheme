bca656ab01e9f4187649fa8efee425d03485750e
Elionai Melo <elionai.melo@gmail.com>
2018-10-05 17:08:10 -0300

    Página de portfólio

---
+++ page-portfolio.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Template Name: portfólio
+*/
+?>
+
+<?php get_header('interno'); ?>
+
+
+
+       <div class="container">
+          <div class="row">
+                <div class="col-md-12">
+							
+									
+									<?php 
+										echo do_shortcode("[visual_portfolio id='134' class='']"); 
+										
+									?>	
+
+
+                </div>
+
+          </div>
+       </div>
+        
+        <?php get_template_part( 'content', 'footer' ); ?>
+        
+    </div>
+
+  
+  <?php get_footer(); ?>
