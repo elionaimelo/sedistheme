c6c04dd1fb4341fdaf6a71bb1db56a9f7abe626f
Elionai Melo <elionai.melo@gmail.com>
2018-10-15 08:14:06 -0300

    correção do form

--- functions.php
+++ functions.php
@@ -262,3 +262,4 @@ function wp_custom_breadcrumbs() {
 		}
 		
 	}
+	
--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
-<form role="search" method="get" action="<?php echo home_url('/'); ?>">
+<form method="GET" class="search-form" action="<?php echo home_url('/'); ?>" role="search" novalidate="">
     <div class="input-group pesquisar-input">
-        <input type="text" class="form-control" placeholder="Digite sua pesquisa">
+    <input required="" value="<?php echo get_search_query(); ?>" name="s" title="Pesquisa" placeholder="Pesquisar" class="form-control" type="text">
         <div class="input-group-append">
             <div class="input-group-text"><i class="icon icon_search"></i></div>
         </div>
